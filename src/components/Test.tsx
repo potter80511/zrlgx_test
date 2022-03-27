15b32b8983228475c23d4c4b32f51cdfe1913b31
簡豪江 <potter80511@gmail.com>
2022-03-27 22:35:07 +0800

    add styled component

---
+++ src/components/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  width: 100%;
+`
+
+const Test = () => {
+  return <StyledWrapper>
+    <div>fdsa</div>
+  </StyledWrapper>
+}
+
+export default Test;
