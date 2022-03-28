041158c99a00a2b271e334a83a0ef8af28249c96
簡豪江 <potter80511@gmail.com>
2022-03-28 18:50:14 +0800

    加上route

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import Test from './components/first_component/slices/Test';
 import { Provider } from 'react-redux';
 import store from './store';
 import IndexContainer from './featrures/index/IndexContainer';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Login from './featrures/login/Login';
+// import { Route, Switch } from 'react-router';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <header className="">
+          header
         </header>
-        <IndexContainer/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<IndexContainer/>} />
+            <Route path="/login" element={<Login />} />
+            {/* 當上方的path都沒有被匹配到 <NoMatch> 就會被渲染 */}
+            {/* <Route component={NoMatch} /> */}
+          </Routes>
+        </BrowserRouter>
       </div>
     </Provider>
   );
---
+++ src/featrures/login/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import arrowCircle from '../../../svg/arrow.svg'
+
+// type PostProps = {
+//   post: PostList;
+//   onRegisterClick: () => void;
+// }
+
+const StyledCreatedAt = styled.div`
+  color: #01254F;
+  font-weight: 600;
+  font-size: 14px;
+  margin-bottom: 20px;
+`
+
+const Login = () => {
+  return (
+    <div>login</div>
+  )
+}
+
+export default Login;
